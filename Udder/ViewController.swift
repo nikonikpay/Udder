ad97fbddcc810f20926ec007ffe88044a053783b
ali <niko.nikpay@gmail.com>
2020-08-27 19:12:43 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ugalaxia company on 27/08/2020.
+//  Copyright © 2020 Ugalaxia company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
