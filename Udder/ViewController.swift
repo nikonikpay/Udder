c2d19a229184f4376a66eaff90ba464c0478dbd6
ali <niko.nikpay@gmail.com>
2020-08-27 20:08:15 +0200

    view did disappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillDisappear(_ animated: Bool) {
+        <#code#>
+    }
 
 }
 
